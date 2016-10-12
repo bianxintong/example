60a19e6d6e8f92313faacc1f5461fb065351c81a
xintong.bian <xintong.bian@ipinyou.com>
2016-10-12 17:15:09 +0800

    feat: wrap exception in root logger

--- logging/log4j2/src/main/java/com/bianxintong/logging/log4j2/UsingSlf4j.java
+++ logging/log4j2/src/main/java/com/bianxintong/logging/log4j2/UsingSlf4j.java
@@ -1,12 +1,16 @@
 package com.bianxintong.logging.log4j2;
 
+import java.io.FileNotFoundException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class UsingSlf4j {
 	private static final Logger LOG = LoggerFactory.getLogger(UsingSlf4j.class);
 	
-	public UsingSlf4j(){
-		LOG.debug("debug w/ slf4j");
+	public UsingSlf4j() throws FileNotFoundException{
+		LOG.info("debug w/ slf4j");
+//		LoggerFactory.getLogger("root").info("throw", new FileNotFoundException());
+		throw new FileNotFoundException();
 	}
 }
